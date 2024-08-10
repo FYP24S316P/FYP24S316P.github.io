5be46d93b2a30f03f8ccb55c175b7affaefdebea
weihow-red <165912018+weihow-red@users.noreply.github.com>
2024-08-10 16:26:51 +0800

    button interaction, removed uncessary contents

--- js/contact-form-script.js
+++ js/contact-form-script.js
@@ -53,3 +53,14 @@ function submitMSG(valid, msg){
     }
     $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
 }
+
+function subscribe() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+  
+    // Add the "show" class to DIV
+    x.className = "show";
+  
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
